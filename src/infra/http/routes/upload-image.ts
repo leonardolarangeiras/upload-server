7b1475e020a7e3e5677bb95de932cc6d7ec54732
leonardolarangeiras <leonardoslarangeiras@gmail.com>
2026-04-23 20:00:23 -0300

    feat: config swagger and tests

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,7 +1,26 @@
 import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
-  server.post('/uploads', () => {
-    return 'hello World'
-  })
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image',
+        body: z.object({
+          name: z.string(),
+          password: z.string().optional(),
+        }),
+        response: {
+          201: z.object({ uploadId: z.string() }),
+          409: z
+            .object({ message: z.string() })
+            .describe('Upload alredy exists.'),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ uploadId: 'teste' })
+    }
+  )
 }
