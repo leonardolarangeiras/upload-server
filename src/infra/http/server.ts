496c9c2cd5aa794a9e162de3350d4c095eef5c7c
leonardolarangeiras <leonardoslarangeiras@gmail.com>
2026-04-23 18:35:00 -0300

    feat: config route uploads

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,36 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler) // seta o validator do zod
+server.setSerializerCompiler(serializerCompiler) // seta o serializer do zod
+
+server.setErrorHandler((error, request, reply) => {
+  //verifica se existem erros de validação do zod para não retornar 500
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // Outros erros, enviar para ferramentas de observabilidade
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(uploadImageRoute)
+
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running')
 })
