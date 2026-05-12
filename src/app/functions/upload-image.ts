8257428b09f0e30346d3561cdb1a1cd3fb0c7936
leonardolarangeiras <leonardoslarangeiras@gmail.com>
2026-05-12 19:07:21 -0300

    feat: adiciona error handler either

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -2,6 +2,8 @@ import { Readable } from 'node:stream'
 import { z } from 'zod'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { Either, makeLeft, makeRight } from '@/infra/shared/either'
+import { InvalidFileFormat } from './errors/invalid-file-format'
 
 const uploadImageInput = z.object({
   fileName: z.string(),
@@ -13,11 +15,13 @@ type UploadImageInput = z.input<typeof uploadImageInput>
 
 const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
 
-export async function uploadImage(input: UploadImageInput) {
+export async function uploadImage(
+  input: UploadImageInput
+): Promise<Either<InvalidFileFormat, { url: string }>> {
   const { contentStream, contentType, fileName } = uploadImageInput.parse(input)
 
   if (!allowedMimeTypes.includes(contentType)) {
-    throw new Error('Invalid file format')
+    return makeLeft(new InvalidFileFormat())
   }
 
   // TODO: carregar image R2
@@ -27,4 +31,6 @@ export async function uploadImage(input: UploadImageInput) {
     remoteKey: fileName,
     remoteUrl: fileName,
   })
+
+  return makeRight({ url: '' })
 }
