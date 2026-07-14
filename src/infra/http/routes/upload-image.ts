d58f90d3b8c4e3e0af93d8f24efd9346401d48c8
leonardolarangeiras <leonardoslarangeiras@gmail.com>
2026-07-14 20:22:46 -0300

    feat: add test factory and get uploads route

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -9,6 +9,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
     {
       schema: {
         summary: 'Upload an image',
+        tags: ['uploads'],
         consumes: ['multipart/form-data'],
         response: {
           201: z.object({ uploadId: z.string() }),
@@ -32,6 +33,10 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         contentType: uploadedFile.mimetype,
         contentStream: uploadedFile.file,
       })
+
+      if (uploadedFile.file.truncated) {
+        return reply.status(400).send({ message: 'File is too large.' })
+      }
 
       if (isRight(result)) {
         console.log(unwrapEither(result))
